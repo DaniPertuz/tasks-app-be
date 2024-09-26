ac6938c7b5685c98cd9e97d0ee916d77acb5f968
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-26 00:22:31 -0500

    App server configuration

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import { MongoDatabase } from './data/mongo';
+import { envs } from './plugins';
+import { AppRoutes } from './presentation/routes';
+import { Server } from './presentation/server';
+
+(async () => {
+  main();
+})();
+
+async function main() {
+
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  });
+
+  const server = new Server({
+    port: envs.PORT,
+    public_path: envs.PUBLIC_PATH,
+    routes: AppRoutes.routes
+  });
+
+  server.start();
+}
