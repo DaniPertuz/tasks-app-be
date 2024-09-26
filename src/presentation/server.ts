6839958ef75422589d25483eb322b3f54e595e33
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-26 00:21:03 -0500

    Server configuration

---
+++ src/presentation/server.ts
@@ -0,0 +1,39 @@
+import express, { Router } from 'express';
+import path from 'path';
+import { ServerOptions } from '../interfaces';
+
+export class Server {
+  private app = express();
+  private readonly port: number;
+  private readonly publicPath: string;
+  private readonly routes: Router;
+
+  constructor(options: ServerOptions) {
+    const { port, public_path = 'public', routes } = options;
+    this.port = port;
+    this.publicPath = public_path;
+    this.routes = routes;
+  }
+
+  async start() {
+
+    // Middlewares
+    this.app.use(express.json()); // raw
+
+    // Public folder
+    this.app.use(express.static(this.publicPath));
+
+    // Routes
+    this.app.use(this.routes);
+
+    // SPA
+    this.app.get('*', (req, res) => {
+      const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
+      res.sendFile(indexPath);
+    });
+
+    this.app.listen(this.port, () => {
+      console.log(`Server runnning on port ${this.port}`);
+    });
+  }
+}
