9e4b4e3fe2234a98267c852003656f2126f871f4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-26 00:19:21 -0500

    Task entity

---
+++ src/domain/entities/task.ts
@@ -0,0 +1,22 @@
+import { ITask, TaskStatus } from '../../interfaces';
+
+export class TaskEntity {
+  public id?: number;
+  public title: string;
+  public body: string;
+  public status?: TaskStatus;
+
+  constructor(options: ITask) {
+    const { id, title, body, status } = options;
+    this.id = id;
+    this.title = title;
+    this.body = body;
+    this.status = status;
+  }
+
+  static fromObject = (object: { [key: string]: any; }): TaskEntity => {
+    const { id, title, body, status } = object;
+    const task = new TaskEntity({ id, title, body, status });
+    return task;
+  };
+}
