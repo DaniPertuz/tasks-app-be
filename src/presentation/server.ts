7b8192f7bf73e93c9fccdafba4caba339ef9465c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-09-30 23:13:52 -0500

    Include all interfaces

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -32,7 +32,7 @@ export class Server {
       res.sendFile(indexPath);
     });
 
-    this.app.listen(this.port, () => {
+    this.app.listen(this.port, '0.0.0.0', () => {
       console.log(`Server runnning on port ${this.port}`);
     });
   }
